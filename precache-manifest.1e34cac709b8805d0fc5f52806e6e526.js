021355e740efc3c0412db497d8d7e043fe149fa4
blueDogs <danielchicchon@gmail.com>
2019-08-09 07:57:51 -0700

    Updates

---
+++ precache-manifest.1e34cac709b8805d0fc5f52806e6e526.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59a5829d2ad8dbe2e75a359e62aba667",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1d5301647d2e094f3ab",
+    "url": "/static/css/2.ad9eb4b5.chunk.css"
+  },
+  {
+    "revision": "4a57cd1a62c5d9f4321d",
+    "url": "/static/css/main.a35cedd5.chunk.css"
+  },
+  {
+    "revision": "d1d5301647d2e094f3ab",
+    "url": "/static/js/2.003f7d63.chunk.js"
+  },
+  {
+    "revision": "4a57cd1a62c5d9f4321d",
+    "url": "/static/js/main.e82c6f91.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9b5fdfdd6b72872cf55574b34302eae6",
+    "url": "/static/media/bg10.9b5fdfdd.jpg"
+  },
+  {
+    "revision": "5c4cfaa3613dc786dbde374d3c6a7f6f",
+    "url": "/static/media/bg4.5c4cfaa3.jpg"
+  },
+  {
+    "revision": "a12e6bc96cad2a081e98b5e1c1a5ff6b",
+    "url": "/static/media/bg9.a12e6bc9.jpg"
+  }
+]);
